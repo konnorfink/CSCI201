ff566e47daec66bc5cadee9a97636473a4595b6b
Konnor Colson <klcolson@middlebury.edu>
2026-09-18 18:31:59 -0400

    Testing Lab 01

---
+++ src/lab01/HelloWorld.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * CSCI 201 - Lab 1.
+ *
+ * @author Konnor Colson
+ */
+
+public class HelloWorld {
+// TODO: define main method that prints "Hello, world!"
+
+    public static void main() {
+        System.out.println("Hello, world!");
+    }
+}
